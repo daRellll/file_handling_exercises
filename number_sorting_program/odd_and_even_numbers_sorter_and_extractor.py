db100cb8c5fc9e519ba767001ac05316eab5cf2d
daRellll <darylljohngsabanal@gmail.com>
2026-04-29 20:54:09 +0800

    moved the odd and even number lists at the top, and added a for loop for checking and appending odd or even numbers into the empty lists

--- number_sorting_program/odd_and_even_numbers_sorter_and_extractor.py
+++ number_sorting_program/odd_and_even_numbers_sorter_and_extractor.py
@@ -3,6 +3,9 @@ with open("numbers.txt", "r") as number_file:
     for line in number_file:
         numbers.append(int(line.strip()))
 
+odd_numbers = []
+even_numbers = []
+
 if len(numbers) == 20:
     print(f"Confirmed! {len(numbers)} are in the file, now sorting...")
 elif len(numbers) < 20:
@@ -10,5 +13,8 @@ elif len(numbers) < 20:
 else:
     print(f"Error! {len(numbers)} is too little! Add more numbers in numbers.txt file to continue")
 
-odd_numbers = []
-even_numbers = []
+for number in numbers:
+    if number % 2 == 0:
+        even_numbers.append(number)
+    else:
+        odd_numbers.append(number)
