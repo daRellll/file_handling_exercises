2c87afa19d3989b3370aba06ee745823d246d285
daRellll <darylljohngsabanal@gmail.com>
2026-04-29 21:10:27 +0800

    fixed a small error about the Error dialogues being switched

--- number_sorting_program/odd_and_even_numbers_sorter_and_extractor.py
+++ number_sorting_program/odd_and_even_numbers_sorter_and_extractor.py
@@ -8,7 +8,7 @@ even_numbers = []
 
 if len(numbers) == 20:
     print(f"Confirmed! {len(numbers)} are in the file, now sorting...")
-elif len(numbers) < 20:
+elif len(numbers) > 20:
     print(f"Error! {len(numbers)} is too much! Delete some lines in numbers.txt file to continue")
 else:
     print(f"Error! {len(numbers)} is too little! Add more numbers in numbers.txt file to continue")
