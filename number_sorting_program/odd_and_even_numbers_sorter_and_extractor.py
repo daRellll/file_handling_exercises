e2da2a15f4266a6e25cb36dec378d92f2eee867f
daRellll <darylljohngsabanal@gmail.com>
2026-04-29 20:51:07 +0800

    added Confirm and Error dialogue for when there is too little or too much numbers

--- number_sorting_program/odd_and_even_numbers_sorter_and_extractor.py
+++ number_sorting_program/odd_and_even_numbers_sorter_and_extractor.py
@@ -2,3 +2,11 @@ with open("numbers.txt", "r") as number_file:
     numbers = []
     for line in number_file:
         numbers.append(int(line.strip()))
+
+if len(numbers) == 20:
+    print(f"Confirmed! {len(numbers)} are in the file, now sorting...")
+elif len(numbers) < 20:
+    print(f"Error! {len(numbers)} is too much! Delete some lines in numbers.txt file to continue")
+else:
+    print(f"Error! {len(numbers)} is too little! Add more numbers in numbers.txt file to continue")
+
