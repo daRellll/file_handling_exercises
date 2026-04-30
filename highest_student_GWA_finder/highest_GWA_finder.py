dfba083deb1fc918167403d5145e01a5d00e60cf
daRellll <darylljohngsabanal@gmail.com>
2026-05-01 02:04:45 +0800

    fixed the print to be more presentable

--- highest_student_GWA_finder/highest_GWA_finder.py
+++ highest_student_GWA_finder/highest_GWA_finder.py
@@ -8,4 +8,4 @@ with open("student_GWA_list.txt", "r") as student_GWA_file:
 top_student = min(student_and_GWA_list, key=lambda x: x[1])
 student_name, GWA = top_student
 
-print(min(student_and_GWA_list, key=lambda x: x[1]))
+print(f"The top student is {student_name} with a GWA of {GWA}!")
