e67f75624555e1d0a93cbcc7e3b2f6b39951cdca
daRellll <darylljohngsabanal@gmail.com>
2026-05-01 02:02:49 +0800

    placed the found top student in a variable

--- highest_student_GWA_finder/highest_GWA_finder.py
+++ highest_student_GWA_finder/highest_GWA_finder.py
@@ -5,4 +5,7 @@ with open("student_GWA_list.txt", "r") as student_GWA_file:
           student_name, GWA = line.strip().split(",")
           student_and_GWA_list.append([student_name, float(GWA)])
 
+top_student = min(student_and_GWA_list, key=lambda x: x[1])
+
+
 print(min(student_and_GWA_list, key=lambda x: x[1]))
