52d5c2dcf25324ab7d03423f99939f083a3c7065
daRellll <darylljohngsabanal@gmail.com>
2026-04-30 23:27:38 +0800

    added a print that outputs the lowest number (best GWA) by using min along woith lambda to only compare the GWA).

--- highest_student_GWA_finder/highest_GWA_finder.py
+++ highest_student_GWA_finder/highest_GWA_finder.py
@@ -4,3 +4,5 @@ with open("student_GWA_list.txt", "r") as student_GWA_file:
      for line in student_GWA_file:
           student_name, GWA = line.strip().split(",")
           student_and_GWA_list.append([student_name, float(GWA)])
+
+print(min(student_and_GWA_list, key=lambda x: x[1]))
