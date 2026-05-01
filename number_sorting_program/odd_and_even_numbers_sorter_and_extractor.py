c985020f792ccca62ec6a2bd28060f1d62f64645
daRellll <darylljohngsabanal@gmail.com>
2026-05-01 11:04:17 +0800

    moved a few lines of codes again into a method for OOP, secifically the codes that detects how many numbers are in the file, and the code that sorts the numbers into an odd and even list

--- number_sorting_program/odd_and_even_numbers_sorter_and_extractor.py
+++ number_sorting_program/odd_and_even_numbers_sorter_and_extractor.py
@@ -10,18 +10,22 @@ class OddAndEvenNumbersSorter:
             for line in number_file:
                 self.numbers.append(int(line.strip()))
 
-if len(numbers) == 20:
-    print(f"\033[92mConfirmed! \033[0m{len(numbers)} numbers are in the file, now sorting...")
-elif len(numbers) > 20:
-    print(f"\033[91mError! \033[0m{len(numbers)} numbers is too much! Delete some lines in numbers.txt file to continue")
-else:
-    print(f"\033[91mError! \033[0m{len(numbers)} numbers is too little! Add more numbers in numbers.txt file to continue")
+    def odd_and_even_numbers_sorter(self):
+        if len(self.numbers) == 20:
+            print(f"\033[92mConfirmed! \033[0m{len(self.numbers)} numbers are in the file, now sorting...")
+            for number in self.numbers:
+                if number % 2 == 0:
+                    self.even_numbers.append(number)
+                else:
+                    self.odd_numbers.append(number)
 
-for number in numbers:
-    if number % 2 == 0:
-        even_numbers.append(number)
-    else:
-        odd_numbers.append(number)
+        elif len(self.numbers) > 20:
+            print(
+                f"\033[91mError! \033[0m{len(self.numbers)} numbers is too much! Delete some lines in numbers.txt file to continue")
+        else:
+            print(
+                f"\033[91mError! \033[0m{len(self.numbers)} numbers is too little! Add more numbers in numbers.txt file to continue")
+
 
 with open("odd.txt", "w") as odd_number_file:
     for number in odd_numbers:
