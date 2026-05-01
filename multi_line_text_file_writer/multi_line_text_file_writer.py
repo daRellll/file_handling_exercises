ee3f78a89577d18cb8e9f1be3edbab863649878e
daRellll <darylljohngsabanal@gmail.com>
2026-05-01 09:29:52 +0800

    called the method inside the while loop and fixed the method name

--- multi_line_text_file_writer/multi_line_text_file_writer.py
+++ multi_line_text_file_writer/multi_line_text_file_writer.py
@@ -2,7 +2,7 @@ class MyLifeWriter:
     def __init__(self, file_name):
         self.file_name = file_name
 
-    def user_write(self, text):
+    def write_line(self, text):
         with open(self.file_name, 'a') as file:
             text = input("Enter line: ")
             file.write(text + "\n")
@@ -10,4 +10,4 @@ class MyLifeWriter:
 my_life_file = MyLifeWriter("my_life.txt")
 
 while True:
-    
+    my_life_file.write_line()
