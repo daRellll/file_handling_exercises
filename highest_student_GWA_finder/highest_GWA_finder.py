4c4c9ad6dc56dc4dabf9b88baf2dbe754c76a1c5
daRellll <darylljohngsabanal@gmail.com>
2026-05-01 10:50:22 +0800

    fixed several bugs regarding the student_and_GWA_list not having a .self at the start, and also converted the line of code that finds the top students through the list using their GWA into a method for OOP

--- highest_student_GWA_finder/highest_GWA_finder.py
+++ highest_student_GWA_finder/highest_GWA_finder.py
@@ -1,15 +1,17 @@
 class GWAAnalyzer:
     def __init__(self, file_name):
         self.file_name = file_name
-        student_and_GWA_list = []
+        self.student_and_GWA_list = []
 
     def file_data_loader (self):
          with open(self.file_name, "r") as student_GWA_file:
               for line in student_GWA_file:
                    student_name, GWA = line.strip().split(",")
-                   student_and_GWA_list.append([student_name, float(GWA)])
+                   self.student_and_GWA_list.append([student_name, float(GWA)])
 
-top_student = min(student_and_GWA_list, key=lambda x: x[1])
-student_name, GWA = top_student
+    def top_student_finder(self):
+          top_student = min(self.student_and_GWA_list, key=lambda x: x[1])
+          student_name, GWA = top_student
+          return student_name, GWA
 
 print(f"The top student is \033[33m{student_name}\033[0m with a GWA of \033[94m{GWA}\033[0m!")
