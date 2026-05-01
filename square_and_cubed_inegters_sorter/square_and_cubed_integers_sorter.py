101f5517f3503247941973702a8ff354d412654e
daRellll <darylljohngsabanal@gmail.com>
2026-05-01 11:28:30 +0800

    created a class to hold my methods for sorting and transforming integer numbers

---
+++ square_and_cubed_inegters_sorter/square_and_cubed_integers_sorter.py
@@ -0,0 +1,7 @@
+class OddAndEvenNumbersSorterAndTransformer:
+    def __init__(self, integers_file):
+        self.integers_file = integers_file
+        self.numbers = []
+        self.odd_numbers = []
+        self.even_numbers = []
+
